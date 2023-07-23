1f48b16f37892e101a6040c0a7b923dee81108cc
gabriele <gabrieletorta@gmail.com>
2023-07-23 18:15:35 +0200

    project1 done

--- App.js
+++ App.js
@@ -1,43 +1,78 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import vibrate from "./utils/vibrate";
 
 //start timer 25, than switch to 5
 //25 min = 1500 seconds, 5 mins = 300 seconds
 
-const Timer = () => {
-  const [counter, setCounter] = useState(20);
+const Timer = ({ studyTime, breakTime }) => {
+  const [counter, setCounter] = useState(studyTime);
   const [switchTimer, setSwitchTimer] = useState(false);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
-    vibrate();
     let internalCounter;
     if (switchTimer) {
-      internalCounter = 10;
-      setCounter(10);
+      if (counter != 0 && isResetting === false) {
+        internalCounter = counter;
+      } else {
+        internalCounter = breakTime;
+        setCounter(breakTime);
+        setIsResetting(false);
+      }
     } else {
-      internalCounter = 20;
-      setCounter(20);
+      if (counter != 0 && isResetting === false) {
+        internalCounter = counter;
+      } else {
+        internalCounter = studyTime;
+        setCounter(studyTime);
+        setIsResetting(false);
+      }
     }
     const interval = setInterval(() => {
-      internalCounter--;
-      if (internalCounter >= 0) {
-        setCounter((prevCounter) => prevCounter - 1);
-      } else {
-        clearInterval(interval);
-        setSwitchTimer((prevState) => !prevState);
+      if (!isPaused) {
+        internalCounter--;
+        if (internalCounter >= 0) {
+          setCounter((prevCounter) => prevCounter - 1);
+        } else {
+          clearInterval(interval);
+          vibrate();
+          setSwitchTimer((prevState) => !prevState);
+        }
       }
     }, 1000);
     return () => clearInterval(interval);
-  }, [switchTimer]);
+  }, [switchTimer, isResetting, isPaused]);
 
-  return <Text>{counter}</Text>;
+  return (
+    <View>
+      <Text>{counter}</Text>
+      <Button title="pause" onPress={() => setIsPaused((prevState) => !prevState)} />
+      <Button title="reset" onPress={() => setIsResetting(true)} />
+    </View>
+  );
 };
 
 export default App = () => {
+  const [isTimerStopped, setIsTimerStopped] = useState(true);
+  const [studyTime, setStudyTime] = React.useState("");
+  const [breakTime, setBreakTime] = React.useState("");
+
   return (
     <View style={styles.container}>
-      <Timer></Timer>
+      {isTimerStopped ? (
+        <View>
+          <TextInput placeholder="study time" onChangeText={setStudyTime} value={studyTime} />
+          <TextInput placeholder="break time" onChangeText={setBreakTime} value={breakTime} />
+          <Button title="start pomodoro" onPress={() => setIsTimerStopped(false)}></Button>
+        </View>
+      ) : (
+        <View>
+          <Timer studyTime={studyTime} breakTime={breakTime}></Timer>
+          <Button title="stop" onPress={() => setIsTimerStopped(true)} />
+        </View>
+      )}
     </View>
   );
 };
